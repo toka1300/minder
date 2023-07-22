9c67ec053c28220f286eac596396f091a8223262
Casey Tokarchuk <105597527+toka1300@users.noreply.github.com>
2023-07-22 15:06:55 -0400

    Render update 9

--- render-build.sh
+++ render-build.sh
@@ -5,6 +5,4 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
-
-#if you have seeds to run add:
-# bundle exec rails db:seed
+bundle exec rails db:seed
